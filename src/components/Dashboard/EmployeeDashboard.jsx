757b8a23e61cc9edfb864525fe08fe1241ccd924
M-Aitisam <aitisamahmed24@gmail.com>
2024-11-25 01:05:56 +0500

    ADD the beautiful  cardTask it show infinity card thought this also i  EmployeeDashboardTaskList

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../../others/header'    
 import CardTask from '../../others/CardTask'    
-
+import EmployeeDashboardTaskList from '../../others/EmployeeDashboardTaskList'
 const EmployeeDashboard = () => {
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen '>
      <Header />
      <CardTask />
+     <EmployeeDashboardTaskList />
     </div>
   )
 }
