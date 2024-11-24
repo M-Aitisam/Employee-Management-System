c97f12ed835199a9a430b1a5c245d5dba87dec8e
M-Aitisam <aitisamahmed24@gmail.com>
2024-11-24 23:44:42 +0500

    add the components/other folder  and inside we create the Card Task  and attach component/Dashboard/ EmployeeDashboard

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../others/header'    
+import CardTask from '../../others/CardTask'    
 
 const EmployeeDashboard = () => {
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen '>
      <Header />
+     <CardTask />
     </div>
   )
 }
