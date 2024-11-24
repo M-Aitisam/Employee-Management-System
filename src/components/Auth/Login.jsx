baa7bad05fb1acd7b8257d2688bf6b5a47544b35
M-Aitisam <aitisamahmed24@gmail.com>
2024-11-24 22:08:21 +0500

    Work  on component/Auth/ LoginPage also  add the Styling of the login  in  the App.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div className='flex items-center justify-center'>
-        <div className='border-2 border-red-600'>
-
-            <form>
-<input type="email" placeholder='Enter Your Email' />
-<input type="password" placeholder='Enter Password' />
-            </form>
-        </div>
- 
+    <div className="flex  h-screen w-screen items-center justify-center">
+      <div className="border-2 rounded-xl border-emerald-600 p-20">
+        <form className="flex flex-col items-center justify-center ">
+          <input required className="  outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-grey-400" type="email" placeholder="Enter Your Email" />
+          <input required  className="  outline-none bg-transparent border-2 mt-3 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-grey-400" type="password" placeholder="Enter Password" />
+          <button className="  mt-5 text-white outline-none  border-none bg-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-white" >Log in</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
