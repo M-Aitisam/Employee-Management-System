3190f2b192510eaa6d533af6c00c94029e65f8e3
M-Aitisam <aitisamahmed24@gmail.com>
2024-11-25 13:16:00 +0500

    Update the code /other/AdminForm complete the Admin Dashboard

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../../others/header"
 import AdminForm from "../../others/AdminForm";
+import AdminDashboardAllTask from "../../others/AdminDashboardAllTask"
+
+
 const AdminDashboard = () => {
   return (
     <div className="h-screen w-full p-7">
       <Header />
        <AdminForm />  
-     
+     <AdminDashboardAllTask />
     </div>
   );
 };
---
+++ src/others/AdminDashboardAllTask.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+
+const AdminDashboardAllTask = () => {
+  return (
+    <div id="AdminDashboardAllTask" className="bg-[#1c1c1c] p-5 mt-5  h-64 overflow-auto " >
+      {/* Task1 */}
+      <div className="bg-teal-500 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task2 */}
+      <div className="bg-green-500 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task3 */}
+      <div className="bg-orange-500 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task4 */}
+      <div className="bg-rose-800 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task5 */}
+      <div className="bg-pink-500 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task6 */}
+      <div className="bg-gray-500 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task7 */}
+      <div className="bg-green-400 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task8 */}
+      <div className="bg-lime-500 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task9 */}
+      <div className="bg-slate-500 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task10 */}
+      <div className="bg-teal-700 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task11 */}
+      <div className="bg-teal-500 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task12 */}
+      <div className="bg-green-500 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task13 */}
+      <div className="bg-orange-500 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task14 */}
+      <div className="bg-rose-800 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+      {/* Task15 */}
+      <div className="bg-pink-500 py-3 font-semibold px-4 rounded mb-2 flex justify-between">
+        <h1>Aitisam</h1> 
+        <h3>Make a UI Design</h3> 
+        <h5>Status</h5> 
+      </div>
+
+    </div>
+  );
+};
+
+export default AdminDashboardAllTask;
